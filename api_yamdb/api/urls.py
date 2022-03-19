5f7b305697cfc68281d3723b417652716e69b446
Василий Исиченко <isvanik66@yandex.ru>
2022-03-19 10:58:56 +0200

    исправил ошибки в путях

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+app_name = "api"
+
+
+v1_router = DefaultRouter()
+
+urlpatterns = [
+    path("v1/", include(v1_router.urls)),
+]
