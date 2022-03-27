7a6f03a7c9cf5208a9ba0f559164c845124f9502
xDDoS <serzh.10@yandex.ru>
2022-03-27 17:15:33 +0300

    pep8

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from .validators import validation_of_the_year
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import UniqueConstraint
+
+from .validators import validation_of_the_year
 
 
 class UserRoles:
